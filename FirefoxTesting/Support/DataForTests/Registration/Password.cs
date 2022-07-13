ea3ec24d5ac71c5ff95d05e1cd3250eeaabd6362
Glareforme <91750407+Glareforme@users.noreply.github.com>
2022-07-13 22:09:16 +0300

    update dataGenerate

--- FirefoxTesting/Support/DataForTests/Registration/Password.cs
+++ FirefoxTesting/Support/DataForTests/Registration/Password.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Security.Cryptography;
 
 namespace NewModelTestFireFox.Support.DataForTests.Registration
 {
-    class Password
+    internal class Password
     {
+        internal static string GeneratePassword()
+        {
+            return null;
+        }
+        internal static string ConfirmPassword() 
+        {
+            return GeneratePassword();
+        }
     }
 }
